efc91e4d6c72e350eb2516c2ae27273f27deee03
willywillywill <43361233asd@gmail.com>
2023-08-25 16:56:32 +0800

    8/25

---
+++ 8_25/a223.py
@@ -0,0 +1,5 @@
+
+
+s = "AABBCCDDD"
+ls = len(s)
+ls = [ i for i in range(1,ls+1) if ls%i==0 ]
--- 8_25/c086.py
+++ 8_25/c086.py
@@ -6,15 +6,15 @@ while 1:
         n = str1.pop(0)  #多少人參加樂透
         x = str1.pop(0)  #多少人可以休假
 
-        n = list((range(1, n+1)))
+        n = list((range(1, n+1)))  #建立一個從 1~n+1 的矩陣用來記錄參加樂透的人
 
-        while len(n) > x:
-            val = str1.pop(0)
-            k = n[val-1::val]
-            del n[val-1::val]  
+        while len(n) > x:      #刪到參加人數 < 休假人數
+            val = str1.pop(0)  #要被刪除的 index
+            k = n[val-1::val]  #紀錄被刪除的數
+            del n[val-1::val]  #刪除數
             
-        if len(n) != x:
-            n.append(k[::-1])
+        if len(n) != x:        #如果多刪除
+            n.append(k[::-1])  #從紀錄中提取被刪除的數
             n.sort()
         print("Selection #%d"%(g))
         g+=1
