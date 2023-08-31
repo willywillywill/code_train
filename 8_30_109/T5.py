bfd5b0713529ec7c9d048df8394951d6b429a8d9
willywillywill <43361233asd@gmail.com>
2023-08-31 16:43:08 +0800

    OK

--- 8_30_109/T5.py
+++ 8_30_109/T5.py
@@ -2,11 +2,30 @@
 stack = []
 s = list(input())
 arr = []
+k = []
 while s:
     ss = s.pop(0)
+
     if ss == "(":
-        if stack[-1] == ")":
-            pass
+        stack.append(ss)
+        arr.append("*")
+        k.append(len(arr)-1)
     elif ss == ")":
-        if stack[-1] == "(":
+        if stack:
             stack.pop(-1)
+            arr.append("*")
+            k.pop(0)
+        else:
+            arr.append("?")
+    else:
+        arr.append("=")
+    print(stack)
+for i in k:
+    arr[i] = "?"
+print("".join(arr))
+
+
+"""
+(A+B)(C+D))+A-B(D+E)(T
+((A+5)+(B-3)/2))
+"""
