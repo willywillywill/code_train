8229c9560cec871211c11069d92e5412ecdd2d33
willywillywill <43361233asd@gmail.com>
2023-09-05 16:15:25 +0800

    OK

--- 9_4/P_6_104.py
+++ 9_4/P_6_104.py
@@ -1,5 +1,5 @@
 import pprint
-
+import numpy as np
 n = list(map(int, input().split(",")))
 
 A = []
@@ -7,51 +7,23 @@ B = []
 AB = []
 xy = {}
 for i in range(n[0]):
-    k = list(map(int, input().split()))
+    k = list(map(str, input().split()))
     if 9999 in k:
         xy["A"] = [i,k.index(9999)]
     A.append(k)
 for i in range(n[2]):
-    k = list(map(int, input().split()))
+    k = list(map(str, input().split()))
     if 9999 in k:
         xy["B"] = [i,k.index(9999)]
     B.append(k)
 for i in range(n[0]):
-    k = list(map(int, input().split()))
+    k = list(map(str, input().split()))
     if 9999 in k:
         xy["AB"] = [i,k.index(9999)]
     AB.append(k)
 
 
-if "A" in xy:
-    ans = {}
-    a = []
-    for i in range(len(AB[xy["A"][0]])):
-        ans[ AB[xy["A"][0] ][i] ] = []
-        for j in range(n[1]):
-            c = [A[xy["A"][0]][j], B[j][i]]
-            if 0 not in c and 9999 in c:
-                a = AB[xy["A"][0] ][i]
-            ans[ AB[xy["A"][0] ][i] ].append( c )
-    d = 1
-    sx = 0
-    num = 0
-    for i in ans[a]:
-        if 9999 not in i:
-            sx+= i[0]*i[1]
-        else:
-            num = i[1] if i[0]==9999 else i[0]
-    print(int((a-sx)/num))
-else:
-    ans = {}
-    a = []
-    for i in range(len(AB[xy["B"][0]])):
-        ans[ AB[xy["B"][0] ][i] ] = []
-        for j in range(n[1]):
-            print(A[xy["B"][0]][0], B[j][xy["B"][1]])
 
-print(a)
-print(ans)
 
 """
 2,3,3,4 
