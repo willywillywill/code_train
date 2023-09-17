32b50c546e0477ad259d0580b5b0f9f1339cc0dd
willywillywill <43361233asd@gmail.com>
2023-09-18 05:59:17 +0800

    OK

--- dom111_103/N.py
+++ dom111_103/N.py
@@ -1,9 +1,9 @@
-# LIS https://web.ntnu.edu.tw/~algo/Subsequence.html
+# LIS-dp https://web.ntnu.edu.tw/~algo/Subsequence.html
 
 _ = int(input())
 lst = list(map(int, input().split()))
 n = len(lst)
-length = [ 1 for i in range(n) ]
+length = [ 1 for i in range(n) ] #dp
 
 for i in range(n):
     for j in range(i):
@@ -14,4 +14,12 @@ print(max(length))
 
 """
 20 40 32 67 40 20 89 300 404 13 13
+lst = list(map(int, input().split()))
+dp = [ 1 for i in range(len(lst)) ]
+
+for i in range(len(lst)):
+    for j in range(i):
+        if lst[i] > lst[j]:
+            dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))
 """
