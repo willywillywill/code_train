024d9b61f5718e1792fa1d0a1422115f418c048f
willywillywill <43361233asd@gmail.com>
2023-09-20 01:21:51 +0800

    OK

--- dom_101/C.py
+++ dom_101/C.py
@@ -1,22 +1,22 @@
-"""
+
 def dfs(x):
     for i in range(n):
-        if lst[x][i]:
+        if lst[i][x]:
             d[i] = d[x]+1
             dfs(i)
-
 for _ in range(int(input())):
     n = int(input())
-    lst = [[ 0 for j in range(n) ] for i in range(n)]
+    lst = [[ 0 for j in range(n-1) ] for i in range(n-1)]
     for i in range(n-1):
         m = list(map(int, input().split(',')))
-        lst[m[1]][m[0]] = 1
+        lst[m[0]][m[1]] = 1
     d = [ 1 for i in range(n) ]
     dfs(0)
     print(max(d))
-"""
-dit = {}
-n = int(input())
+    print()
+
+
+
 
 
 """
@@ -31,4 +31,21 @@ n = int(input())
 3,0
 2
 1,0
+
+2
+7
+1,3
+2,3
+3,5
+4,5
+5,0
+6,5
+8
+1,6
+2,3
+3,4
+4,6
+5,6
+6,0
+7,2
 """
