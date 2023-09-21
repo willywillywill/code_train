9c2c9f6d01ae14f66a09162a011ff5b815850edb
willywillywill <43361233asd@gmail.com>
2023-09-21 21:01:47 +0800

    OK

--- dom103/Problem_5.py
+++ dom103/Problem_5.py
@@ -1,14 +1,22 @@
-fib = [0,1]
-st = set()
-n = 6
-for i in range(2,n):
-    fib.append(fib[i-1]+fib[i-2])
+in1 = list(map(int,input().split()))
+in1 = [ (i%14)+(i//14) for i in in1 ]
+same_dit = {
+    2:0,
+    3:0,
+    4:0
+    }
+print(in1)
+while in1:
+    k1 = in1.pop(0)
+    k1_n = 1
+    while (k1 in in1):
+        del in1[in1.index(k1)]
+        k1_n+=1
+    if k1_n in same_dit:
+        same_dit[k1_n]+=1
+print(same_dit)
 
-if n not in fib:
-    for i in range(2,len(fib)):
-        for j in range(2,len(fib)):
-            if fib[i]+fib[j] == n and i!=j:
-                st.add(tuple(sorted([i,j])))
-else:
-    print(fib.index(n))
-print(st)
+"""
+3 44 4 6 7 5 
+19 12 1 32 45 25
+"""
