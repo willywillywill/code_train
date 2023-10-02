8dbb223e507ef19e719180f2a4938a8b456f05b7
willywillywill <43361233asd@gmail.com>
2023-10-02 17:47:20 +0800

    OK

--- dom_108/G.py
+++ dom_108/G.py
@@ -1,29 +1,47 @@
-dit = {}
+
+
 edge_number = int(input())
+val = [0]* edge_number
+val[0] = 1
+dit = {}
 for i in range(int(input())):
     a,b = list(map(int,input().split()))
     if a in dit:
         dit[a].append(b)
     else:
         dit[a] = [b]
+    if b in dit:
+        dit[b].append(a)
+    else:
+        dit[b] = [a]
 
-print(dit)
+def f(px,x):
+    for i in dit[x]:
+        if i != px and val[i]==0:
+            val[i] = val[x]+1
+            f(x,i)
+
+f(0,0)
+print(val)
 
 """
-3
-3
-0 1
-1 2
-2 0
-
-9
-8
+5
+10
 0 1
 0 2
 0 3
 0 4
-0 5
-0 6
-0 7
-0 8 
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+
+4
+4
+0 1
+1 2
+2 3
+3 0
 """
