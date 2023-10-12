b088238908078313dc0c88dbde97899065535a56
willywillywill <43361233asd@gmail.com>
2023-10-12 14:25:55 +0800

    .

--- dom_111_01/111_01E1.py
+++ dom_111_01/111_01E1.py
@@ -1,47 +1,40 @@
 def f_s(s):
-    global max_layer
+    global off
     s = s.replace("(","")
     s = s.replace(")","")
     s = s.split(",")
-    max_layer = max(max_layer, len(s[1]))
-    return s
-def dfs(i):
-    if i-1 > len(visited)-1:
-        return
-    if ans[i-1]:
-        visited[i-1]=1
-        dfs(2*i)
-        dfs(2*i+1)
-    else:
-        return
-while 1:
-    try:
-        max_layer = 0
-        in1 = list(map(f_s, "(5,) (4,R) (3,LL) ()"[:-2].split()))
-        ans = [0]*(2**(max_layer+1)-1)
+    if s[1] in dit:
+        off = 1
+    dit[s[1]] = int(s[0])
 
-        for i in in1:
-            k = 1
-            for j in i[1]:
-                if j=="L":
-                    k = k*2
-                else:
-                    k = k*2+1
-            if ans[k-1]:
-                print("not complete")
-                break
-            ans[k-1] = i[0]
-        else:
-            visited = [0]*(2**(max_layer+1)-1)
-            if ans[0]:
-                dfs(1)
-                t_ans = [ int(bool(i)) for i in ans ]
-                if t_ans==visited:
-                    ans = [ i for i in ans if i ]
-                    print( " ".join(ans) )
-                else:
-                    print("not complete")
-            else:
-                print("not complete")
-    except:
-        break
+dit = {}
+off = 0
+in1 = list(map(f_s, input().split()[:-1]))
+node_num = len(dit)
+if off or "" not in dit:
+    print("not complete")
+    continue
+queue = [""]
+visited_num = 0
+out = []
+while queue:
+    node = queue.pop(0)
+    visited_num += 1
+    out.append(str(dit[node]))
+    del dit[node]
+    if node+"L" in dit.keys():
+        queue.append(node+"L")
+    if node+"R" in dit.keys():
+        queue.append(node+"R")
+print(visited_num,node_num)
+if visited_num != node_num:
+    print("not complete")
+else:
+    print(" ".join(out))
+
+"""
+(11,LL) (7,LLL) (8,R) (5,) (4,L) (13,RL) (2,LLR) (1,RRR) (4,RR) ()
+(3,L) (4,R) ()
+(11,LL) (7,LLL) (2,LLL) (8,R) (5,) (4,L) (13,RL) (2,LLR) (1,RRR) (4,RR) ()
+(5,) (3,L) (4,R) ()
+"""
--- dom_111_01/111_01E1_2.py
+++ dom_111_01/111_01E1_2.py
@@ -1,45 +1,48 @@
 def f_s(s):
-    global max_layer,val,idx
+    global max_layer
     s = s.replace("(","")
     s = s.replace(")","")
     s = s.split(",")
+    max_layer = max(max_layer, len(s[1]))
     k=1
     for i in s[1]:
         if i=="L":
             k = k*2
         else:
             k = k*2+1
-    max_layer = max(max_layer, len(s[1]))
-    #val.append(s[0])
-    #idx.append(k)
+    s[1]=k
     return s
-
 def dfs(i):
-    if i in idx:
-        visited[i-1]=str(val[idx.index(i)])
+    if i-1 > len(visited)-1:
+        return
+    if ans[i-1]:
+        visited[i-1]=1
         dfs(2*i)
         dfs(2*i+1)
-
+    else:
+        return
 while 1:
     try:
         max_layer = 0
-        val = []
-        idx = []
-        list(map(f_s, input()[:-2].split()))
-        visited = [0]*(2**(max_layer+1)-1)
-        dfs(1)
-        visited = [ i for i in visited if i ]
-        if len(visited) == len(val):
-            print(" ".join(visited))
+        in1 = list(map(f_s, input()[:-2].split()))
+        ans = [0]*(2**(max_layer+1)-1)
+
+        for i in in1:
+            if ans[i[1]-1]:
+                print("not complete")
+                break
+            ans[i[1]-1] = i[0]
         else:
-            print("not complete")
+            visited = [0]*(2**(max_layer+1)-1)
+            if ans[0]:
+                dfs(1)
+                t_ans = [ int(bool(i)) for i in ans ]
+                if t_ans==visited:
+                    ans = [ i for i in ans if i ]
+                    print( " ".join(ans) )
+                else:
+                    print("not complete")
+            else:
+                print("not complete")
     except:
         break
-
-"""
-(11,LL) (7,LLL) (8,R) (5,) (4,L) (13,RL) (2,LLR) (1,RRR) (4,RR) ()
-(3,L) (4,R) ()
-(11,LL) (7,LLL) (2,LLL) (8,R) (5,) (4,L) (13,RL) (2,LLR) (1,RRR) (4,RR) ()
-
-(5,) (3,RR) (4,R) ()
-"""
