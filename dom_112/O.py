1f9c793a9408a5ed80b55ca61cd387eb5e7fed58
willywillywill <43361233asd@gmail.com>
2023-10-18 21:17:33 +0800

    .

--- dom_112/O.py
+++ dom_112/O.py
@@ -1,19 +1,12 @@
-
+# https://www.youtube.com/watch?v=XqoVyBKEhac
 while 1:
     try:
         p,d = list(map(int,input().split()))
-        n=0
-        for i in range(1,int(d**0.5)+1):
-            if d%i==0:
-                n = i
-        m = d//n
-        print(n,m)
-        for i in range(n,m+1):
-            print(i,d,(p+i)*(i-p+1)//2, d-((p+i)*(i-p+1)//2)<=0)
-            #if d-((p+i)*(i-p+1)//2)<=0:
-            #    print(i)
-            #    break
-
+        st = int((d*2)**0.5)
+        for i in range(st,st+st):
+            if (i-p+1)*(i+p) >= 2*d:
+                print(i)
+                break
     except:
         break
 
