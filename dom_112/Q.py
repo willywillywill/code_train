f1b55d131fa4215ac8f216020529fb38f9086acf
willywillywill <43361233asd@gmail.com>
2023-10-19 07:03:03 +0800

    .

--- dom_112/Q.py
+++ dom_112/Q.py
@@ -1,13 +1,29 @@
-"""n,q = list(map(int,input().split()))
-lst = list(range(n))
-for i in range(q):
-    a1,a2 = list(map(int,input().split()))
-    lst[a1-1],lst[a2-1] = lst[a2-1],lst[a1-1]
-    ans=0
-  
-    for j in range(n):
-        for k in range(j):
-            if lst[j]<lst[k]:
-                ans+=1
-    print(ans)
+
+
+
+def query(n):
+    s = 0
+    while len(lst)>n>0:
+        s += bit[n]
+        n -= n&-n
+    return s
+def update(n):
+    while n<len(lst):
+        bit[n]+=lst[n]
+        n += n&-n
+
+lst = [5,2,3,4,1]
+bit = [0]*len(lst)
+ans = 0
+for i in range(len(lst)):
+    ans += i-query(lst[i])
+    update(lst[i])
+print(ans)
+
+"""
+5 4
+4 5
+2 4
+2 5
+2 2
 """
