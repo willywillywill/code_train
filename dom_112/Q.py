d0940e331e306e06896dbf606e032803e16592fd
willywillywill <43361233asd@gmail.com>
2023-10-18 16:34:15 +0800

    .

--- dom_112/Q.py
+++ dom_112/Q.py
@@ -1,17 +1,13 @@
 n,q = list(map(int,input().split()))
 lst = list(range(n))
+for i in range(q):
+    a1,a2 = list(map(int,input().split()))
+    lst[a1-1],lst[a2-1] = lst[a2-1],lst[a1-1]
+    ans=0
 
-a1,a2 = list(map(int,input().split()))
-lst[a1-1],lst[a2-1] = lst[a2-1],lst[a1-1]
-num = 0
-ans = 0
-for i in range(len(lst)):
-    if lst[i] > num:
-        num+=lst[i]
-        ans+=1
-print(ans)
-"""
-5 4
-4 5
-"""
+    for j in range(n):
+        for k in range(j):
+            if lst[j]<lst[k]:
+                ans+=1
+    print(ans)
 
