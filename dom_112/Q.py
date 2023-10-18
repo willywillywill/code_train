a69c4edadbf938f4472eee8528b843806e8a6dc6
willywillywill <43361233asd@gmail.com>
2023-10-18 22:40:48 +0800

    .

--- dom_112/Q.py
+++ dom_112/Q.py
@@ -4,7 +4,7 @@ for i in range(q):
     a1,a2 = list(map(int,input().split()))
     lst[a1-1],lst[a2-1] = lst[a2-1],lst[a1-1]
     ans=0
-
+  
     for j in range(n):
         for k in range(j):
             if lst[j]<lst[k]:
