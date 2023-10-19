7d5e7844d5827acf0e5529d0b42590374bd2e419
willywillywill <43361233asd@gmail.com>
2023-10-19 13:32:08 +0800

    .

--- dom_111_01/111_01E2.py
+++ dom_111_01/111_01E2.py
@@ -4,40 +4,45 @@ for _ in range(int(input())):
     n = int(input())
     m = int(input())
     graph = []
-    visited = [[0 for i in range(m)] for j in range(n)]
+
     for i in range(n):
-        graph.append(list(map(int,input().split())))
-    mxy = [(0,-1),(-1,0),(1,0),(0,1)]
-    heap = []
-    heapq.heappush(heap,(0,0,graph[0][0]))
+        l = list(map(int,input().split()))
+        graph.append(l)
+        
+    visited = [[False for i in range(m)] for i in range(n)]
+    mxy = [(1,0),(0,1),(0,-1),(-1,0)]
+    heap = [(graph[0][0],0,0)]
+    visited[0][0] = 1
     while heap:
-        heap = heapq.nlargest(len(heap),heap,key=lambda x:-x[2])
-        lx,ly,lval = heapq.heappop(heap)
-        if lx==n-1 and ly==m-1:
-            break
-        visited[lx][ly]=1
+        hnode = heapq.heappop(heap)
+        val = hnode[0]
+        x = hnode[1]
+        y = hnode[2]
         for mx,my in mxy:
-            if 0<=lx+mx<n and 0<=ly+my<m:
-                if visited[lx+mx][ly+my] == 0:
-                    l = [ [i[0],i[1]] for i in heap ]
-                    if [lx+mx,ly+my] in l:
-                        idx = l.index([lx+mx,ly+my])
-                        heap[idx][2] = min(heap[idx][2],lval+graph[lx+mx][ly+my])
-                    else:
-                        heapq.heappush(heap,[lx+mx,ly+my,lval+graph[lx+mx][ly+my]])
-    print(lval)
+            nx = mx+x
+            ny = my+y
+            if nx<0 or ny<0 or nx>n-1 or ny>m-1 or visited[nx][ny]:
+                continue
+            graph[nx][ny] = val+graph[nx][ny]
+            visited[nx][ny] = True
+            heapq.heappush(heap, (graph[nx][ny],nx,ny))
+    print(graph[-1][-1])
 
-
-"""
-2
-4
-5
-0 3 1 2 9
-7 3 4 9 9
-1 7 5 5 3
-2 3 4 2 5
+""" 
+3
 1
+1
+9
+2
+2
+0 0
+0 0
+5
 6
-0 1 2 3 4 5
+0 9 9 0 0 0
+0 9 0 0 9 0
+0 9 0 9 9 0
+0 0 0 9 9 0
+9 9 9 9 9 0
+
 """
-            
