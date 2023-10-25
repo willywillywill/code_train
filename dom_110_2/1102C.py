7316106c289ad2fbe2bc3a5c5669e82d3d8a8acc
willywillywill <43361233asd@gmail.com>
2023-10-25 21:31:25 +0800

    .

--- dom_110_2/1102C.py
+++ dom_110_2/1102C.py
@@ -1,12 +1,6 @@
 
 while 1:
     try:
-<<<<<<< HEAD
-        s = list(input())
-        ans = [ "+" if i%2==0 else "-" for i in range(len(s)-1) ]
-        out = [ str(s[i])+str(ans[i]) for i in range(len(ans)) ]
-        print(eval("".join(out)+s[-1]))
-=======
         s = input()
         a = [ "+" if i%2==0 else "-" for i in range(len(s)-1) ]
         ans = []
@@ -15,6 +9,5 @@ while 1:
             ans.append(a[i])
         ans.append(s[-1])
         print(eval("".join(ans)))
->>>>>>> 2891908291cc719ddac87077dbe619babe8d4937
     except:
         break
