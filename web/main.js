79ac6c6f8b02ee0f2e59f11b9fa76d474c1e878b
willywillywill <43361233asd@gmail.com>
2023-10-26 06:45:33 +0800

    .

--- web/main.js
+++ web/main.js
@@ -1,18 +1,6 @@
-var data = [];
-
-function update(){
-    var out = "";
-
-    for(var i=0 ; i < data.length ; i++){
-        out += "<button class='text_sub'>"+data[i]+"</button>";
-    }
-    document.getElementById("text").innerHTML = out;
-
-};
 
 
-function insert(){
-    var txt = document.getElementById("insert_txt").value;
-    data.push(txt);
-    update();
-};
+
+
+
+
