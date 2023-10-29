17fb3da6a1bb44dfe4b3d5d5148c0cecf0c2173a
willywillywill <43361233asd@gmail.com>
2023-10-29 21:41:38 +0800

    .

--- dom_110_2_X/1102N.py
+++ dom_110_2_X/1102N.py
@@ -1,20 +1,15 @@
-def distance(a:str, len_a:int, b:str, len_b:int):
-    if len_a == 0:
-        return len_b
-    if len_b == 0:
-        return len_a
-    cos = 0
-    if a[-1] == b[-1]:
-        cos = 0
-    else:
-        cos = 1
-    
-    re1 = distance(a, len_a-1, b,  len_b) + 1
-    re2 = distance(a, len_a  , b, len_b-1) + 1
-    re3 = distance(a, len_a-1, b, len_b-1) + cos
-    
-    return min(re1,re2,re3)
-        
-a = "horse"
-b = "ros"
-print(distance(a,len(a),b,len(b)))    
+in1 = input()
+in2 = input()
+dp = [ [ 0 for i in range(len(in2)+1) ] for j in range(len(in1)+1)  ]
+dp [0] = [ i for i in range(len(in2)+1) ]
+
+for i in range(len(in1)+1):
+    dp[i][0] = i
+
+for i in range(1, len(in1)+1):
+    for j in range(1, len(in2)+1):
+        if in1[i-1]==in2[j-1]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+print(dp[-1][-1]) 
--- dom_110_2_X/1102P.py
+++ dom_110_2_X/1102P.py
@@ -1,28 +1,22 @@
-n = int(input())
-lst = [0]+list(map(int,input().split()))
-dit = { i:lst[i] for i in range(len(lst))}
-dit[len(dit)] = None
+can = [False]*100001
 
-def f(d:dict,pt):
-    if dit[pt] == None:
-        l = [ dit[i] for i in d ]
-        if sum(l):
-            ans.add(str(sum(l)))
-        return
-    
-    d[pt] = dit[pt]
-    k = 1
-    for i in dit:
-        if i not in d:
-            k = 0
-            f(d.copy(),i)    
+num = int(input())
+data = list(map(int,input().split()))
+max_v = sum(data)
 
-
-ans = set()
-f({},0)
-ans = sorted(list(ans), key=lambda x:int(x))
+can[0] = True
+for i in range(num):
+    for k in range(max_v,data[i]-1,-1):
+        can[k] |= can[k-data[i]]
+ans = []
+for j in range(1, max_v+1):
+    if can[j]:
+        ans.append(str(j))
 print(len(ans))
 print(" ".join(ans))
+
+
+
 """
 4
 4 2 2 5
