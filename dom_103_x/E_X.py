6ea90feb4c66caaf56ef53428219b0f915df73d7
willywillywill <43361233asd@gmail.com>
2023-10-29 15:40:33 +0800

    .

--- dom_103_x/E_X.py
+++ dom_103_x/E_X.py
@@ -1,56 +1,58 @@
-# ~ b159
-
-for _ in range(int(input())):
-    x = list(map(int, input().split()))
-    card = [ 0 for i in range(53) ]
-    card_num = [0 for i in range(15)]
-    card_type = [0,0,0,0]
+n=int(input())
+for i in range(n):
+    x=list(map(int, input().split()))
+    card=[0 for i in range(53)]
+    card_num=[0 for i in range(15)]
+    card_type=[0,0,0,0]
     for a in x:
-        card[a] = 1
-        card_num[(a-1)%13+1] += 1
-        if (a-1)%13+1 == 1:
-            card_num[14] += 1
-        card_type[int((a-1)/13)] += 1
-   
-    point = []
-    cnt = 0
-    max_cnt = 0
-    start = -1
+        card[a]=1
+        card_num[(a-1)%13+1]+=1
+        if ((a-1)%13+1)==1:
+            card_num[14]+=1
+        card_type[int((a-1)/13)]+=1
+
+    point=[]
+    cnt=0
+    max_cnt=0
+    start=-1
     for a in range(1,15):
         if card_num[a]>0:
-            cnt += 1
-            if cnt == 1:
-                start = a
-            if max_cnt < cnt:
-                max_cnt = cnt
+            cnt+=1
+            if cnt==1:
+                start=a
+            if max_cnt<cnt:
+                max_cnt=cnt
             continue
-        cnt = 0
-    if max_cnt >= 5:
-        flag = True
-        type1 = 0
-        for i in range(1,4):
-            if card_type[type1] < card_type[a]:
-                type1 = a
-        start += type1*13
+        cnt=0
+    
+    if max_cnt>=5:
+        flag=True
+        type1=0
+        for a in range(1,4):
+            if card_type[type1]<card_type[a]:
+                type1=a
+        start+=type1*13
 
-        for a in range(start,start+5):
+        for a in range(start, start+5):
             if ((a-1)%13+1)==1:
-                a -= 13
-            if card[a] == 0:
-                flag = False
+                a-=13
+            if card[a]==0:
+                flag=False
                 break
         if flag:
             point.append(7)
         else:
             point.append(4)
-    g1 = 0
-    g2 = 0
+    
+    g1=0
+    g2=0
     for a in range(1,14):
-        if g1==0 and card_num[a] >= 2:
-            g1 = card_num[a]
+        if g1==0 and card_num[a]>=2:
+            g1=card_num[a]
             continue
-        if g1 !=0 and card_num[a] >= 2:
-            g2 = card_num[a]
+        if g1!=0 and card_num[a]>=2:
+            g2=card_num[a]
+
     if g1==4 or g2==4:
         point.append(6)
     elif (g1==3 and g2>=2) or (g1==3 and g2>=2):
@@ -61,16 +63,8 @@ for _ in range(int(input())):
         point.append(2)
     elif g1==2:
         point.append(1)
+
+    if len(point)==0:
+        print(0)
     else:
-        point.append(0)
-
-    print(max(point))
-"""
-2 
-3 44 4 6 7 5 
-19 12 1 32 45 25
-
-2 
-14 16 18 19 20 21 
-4 17 30 43 9 22
-"""
+        print(max(point))
