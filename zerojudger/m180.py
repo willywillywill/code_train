8ae095128e4757396da1ea5a571f29e0923959f5
willywillywill <43361233asd@gmail.com>
2023-11-11 13:04:35 +0800

    .

--- zerojudger/m180.py
+++ zerojudger/m180.py
@@ -1,9 +1,25 @@
 
-txt = list("+-*/")
+txt = list("+*")
 in1,ans = input().split()
 ans = int(ans)
 
-def dfs(n):
-    if n==0:
+def f(l):
+    if len(l) == len(in1)+len(in1)-1:
+        p = "".join(l).replace("/","//")
+        if ans == eval(p):
+            c.append(p)
         return
-    if 
+    
+    if len(l)%2!=0:
+        for i in txt:
+            k = l.copy()
+            k.append(i)
+            f(k.copy())
+    else:
+        for i in in1:
+            k = l.copy()
+            k.append(i)
+            f(k.copy())
+c = []
+f([])
+print(c)
