bd4e34b010ab26d2eb4a1498d41c105852466675
willywillywill <43361233asd@gmail.com>
2023-11-12 21:30:52 +0800

    .

--- test/test.py
+++ test/test.py
@@ -1,25 +1,9 @@
-"""  遞迴函數
-w = [10,20,40]
-mone = 100
 
-def f(l,m,r):
-    if m == 0:
-        print(r)
-        return 0
-    if m <0:
-        return
-    for i in l:
-        if m-i >= 0:
-            o = r.copy()
-            o.append(i)
-            f(l.copy(),m-i,o.copy())
-f(w.copy(),mone,[])
+s = input()
+s = s.replace("V","1")
+s = s.replace("F","0")
+print(eval(s))
+
 """
-w = [10,20,40]
-mone = 100
-dp = [0]*(mone+1)
-dp[0] = 1
-for cost in w:
-    for i in range(cost,mone+1):
-        dp[i] += dp[i-cost]
-print(dp)
+(V|V)&F&(F|V)
+"""
