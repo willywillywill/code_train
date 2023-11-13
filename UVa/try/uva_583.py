129088d53e051418ad1d36f45b67273391644e83
willywillywill <43361233asd@gmail.com>
2023-11-13 14:29:45 +0800

    .

--- UVa/try/uva_583.py
+++ UVa/try/uva_583.py
@@ -1,40 +1,56 @@
-max_n = 500001
+# height 2xLoop  (0.6s)
+def f(n):
+    if n<2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+p = [ i for i in range(50000) if f(i)  ]
+
+""" low  3xLoop  (4.8s)
+max_n = 50000
 p = [0]*max_n
+p[0],p[1] = 1,1
 for i in range(2,int(max_n**0.5)+1):
-    if not p[i]:
+    if p[i]:
         for j in range(i+i,max_n,i):
             p[j] = 1
+p = [ i for i in range(len(p)) if not p[i] ] 
+"""
 
 import sys
 for n in sys.stdin:
     n = int(n)
-    t = False
+
+    l = []
     if n==0:
         break
-    print("{} = ".format(n), end="")
 
+    print( "{} = ".format(n),end="" )
     if n < 0:
-        t = True
-        print(-1,end="")
+        l.append("-1")
         n *= -1
 
-    for i in range(2,max_n):
-        if not p[i]:
-           while n%i==0:
-               n //= i
-               if t:
-                   print(" x ",end="")
-               print(i,end="")
-               t = True
+    for i in range(len(p)):
+        while n%p[i] == 0:
+            n //= p[i]
+            l.append(str(p[i]))
         if n==1:
             break
-    if n != 1:
-        if t:
-            print(" x ",end="")
-        print(n,end="")
-    print()
+    else:
+        l.append(str(n))
+
+    print(" x ".join(l))
+
 
 """
 2047291009
 2147483648
+
+2147483548
+
+2147483647
+
+499979
 """
