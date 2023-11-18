948e46f86d7243bafdec52b627f33f8b6855d348
willywillywill <43361233asd@gmail.com>
2023-11-19 01:28:38 +0800

    .

--- UVa/uva_10004.py
+++ UVa/uva_10004.py
@@ -2,12 +2,17 @@
 class Graph:
     def __init__(self, node_number, edge_number):
         self.adj = [[] for i in range(node_number)]
+        self.node = []
         self.color = [0]*node_number
         for i in range(edge_number):
             a,b = map(int,input().split())
             self.adj[a].append(b)
             self.adj[b].append(a)
-def f(graph, root):
+            if a not in self.node:
+                self.node.append(a)
+            if b not in self.node:
+                self.node.append(b)
+def f(root):
     l = []
     for i in graph.adj[root]:
         l.append(graph.color[i])
@@ -17,9 +22,20 @@ def f(graph, root):
     graph.color[root] = c
     return c
 
-node_num = int(input())
-edge_num = int(input())
-graph = Graph(node_num, edge_num)
+while 1:
+    node_num = int(input())
+    if node_num == 0:
+        break
+    edge_num = int(input())
+    graph = Graph(node_num, edge_num)
+    c = {}
+    for root in graph.node:
+        c[root] = f(root)
+
+    if len(set(c.values()))==2:
+        print("BICOLORABLE.")
+    else:
+        print("NOT BICOLORABLE.")
 
 
 """
