29fe127dd2c76e949bc1780df82558d940cc2e39
willywillywill <43361233asd@gmail.com>
2023-11-27 18:33:28 +0800

    .

--- test/test.py
+++ test/test.py
@@ -1,3 +1,21 @@
-shoplist = ['牛奶', '蛋', '咖啡豆', '西瓜', '鳳梨']
-print(shoplist.index("西瓜"))
-            
+class node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def build(inorder, preorder):
+    if inorder:
+        idx = inorder.index(preorder.pop(-1))
+        root = node(inorder[idx])
+        root.left = build(inorder[:idx], preorder)
+        root.right = build(inorder[idx+1:], preorder)
+        return root
+def preOrder(root):
+    if root:
+        preOrder(root.left)
+        preOrder(root.right)
+        print(root.val)
+
+root = build(list("dbac"),list("abdc"))
+preOrder(root)
