3fb698a239b3526e6fdbf4be1dcccb63298a75d2
willywillywill <43361233asd@gmail.com>
2023-12-21 18:22:24 +0800

    .

--- test/test.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    input()
-    s = sorted(list(map(int,input().split())),reverse=True)
-    mid = len(s)//2
-    ans = sum(s[:mid])
-
-    for i in range(mid, len(s)):
-        ans += s[i]//2
-    print(ans)
